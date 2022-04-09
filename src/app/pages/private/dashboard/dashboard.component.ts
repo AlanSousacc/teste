e7d27d8db3d5e1ffdf83ad904d040d4148b2e077
Gabriel Dias <diassilvag31@gmail.com>
2022-04-09 13:04:39 -0300

    feat(IN-875): lading screen adicionado para aguardar as requisicoes

--- src/app/pages/private/dashboard/dashboard.component.ts
+++ src/app/pages/private/dashboard/dashboard.component.ts
@@ -2,11 +2,14 @@ import { DctfWeb } from './../../../services/dctfweb/dctfweb.service'
 import { Component, OnInit } from '@angular/core'
 import { Paginator } from 'primeng/paginator'
 import { SessionService } from 'src/app/services/global/session.service'
+import { NgxSpinnerService } from 'ngx-spinner'
+import { MessageService} from 'primeng/api'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [MessageService]
 })
 export class DashboardComponent implements OnInit {
   currenpage:any
@@ -17,7 +20,7 @@ export class DashboardComponent implements OnInit {
   idresponsavel = 1
   counttotaldata=1000
   semdados=false
-  constructor (private dctfWebService: DctfWeb, private sessionService: SessionService) { }
+  constructor (private dctfWebService: DctfWeb, private sessionService: SessionService, private spinner: NgxSpinnerService, private messageService: MessageService) { }
 
   async ngOnInit () {
     await this.setUpTable()
@@ -26,6 +29,7 @@ export class DashboardComponent implements OnInit {
   async setUpTable () {
     this.competencias = []
     this.idsetor = 8
+    await this.spinner.show('loading')
     this.dctfWebService.getAllDctfCompetencias().subscribe(
       (competencias: any) => {
         this.counttotaldata = competencias.total
@@ -34,6 +38,11 @@ export class DashboardComponent implements OnInit {
           this.currenpage = competencias
           this.competencias = competencias.data
           this.assertCountItems(competencias)
+          this.spinner.hide('loading')
+        },
+        () => {
+          this.spinner.hide('loading')
+          this.errorMessage('Um erro ocorreu ao se conectar com o servidor.')
         })
       })
   }
@@ -55,19 +64,24 @@ export class DashboardComponent implements OnInit {
     this.competencias = []
   }
 
-  loadNextPage (params: any) {
+  async loadNextPage (params: any) {
     this.currentlast = params.last
     this.last += 10
     params.table.loading = true
     this.competencias = []
+    await this.spinner.show('loading')
     this.dctfWebService.getPageLink(this.currenpage.next_page_url).subscribe(
       (competencias: any) => {
         this.assertCountItems(competencias)
         this.counttotaldata = competencias.total
+        this.spinner.hide('loading')
         competencias.data.forEach((competencia: Paginator | any) => {
           this.currenpage = competencias
           this.competencias.push(competencia)
           params.table.loading = false
+        }, () => {
+          this.spinner.hide('loading')
+          this.errorMessage('Um erro ocorreu ao se conectar com o servidor.')
         })
       })
   }
@@ -82,11 +96,16 @@ export class DashboardComponent implements OnInit {
       this.counttotaldata = competencias.total
       competencias.data.forEach((competencia: Paginator | any) => {
         this.competencias.push(competencia)
+      },
+      () => {
+        this.spinner.hide('loading')
+        this.errorMessage('Um erro ocorreu ao se conectar com o servidor.')
       })
     })
   }
 
-  loadBackPage (params: any) {
+  async loadBackPage (params: any) {
+    await this.spinner.show('loading')
     this.currentlast = params.last
     this.last -= 10
     params.table.loading = true
@@ -94,13 +113,22 @@ export class DashboardComponent implements OnInit {
     this.dctfWebService.getPageLink(this.currenpage.prev_page_url).subscribe(
       (competencias: any) => {
         this.assertCountItems(competencias)
+        this.spinner.hide('loading')
         competencias.data.forEach((competencia: any) => {
           this.counttotaldata = competencias.total
           this.currenpage = competencias
           this.competencias.push(competencia)
           params.table.loading = false
+        },
+        () => {
+          this.spinner.hide('loading')
+          this.errorMessage('Um erro ocorreu ao se conectar com o servidor.')
         })
       }
     )
+  }
+
+  errorMessage (errorMessage: string) {
+    this.messageService.add({ severity: 'error', summary: 'Erro', detail: errorMessage })
   }
 }
